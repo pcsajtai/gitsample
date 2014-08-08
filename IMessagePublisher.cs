30ce99aaafd6d422135c137cd03fe7a109e2d90f
Peter Csajtai <peter.csajtai@hotmail.com>
2014-08-08 20:28:35 +0200

    grgehkulkzilzié

---
+++ IMessagePublisher.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ClassLibrary1
+{
+    public interface IMessagePublisher
+    {
+        void Subscribe<TMessage, TSubcriber>(TSubcriber messageReciever) where TSubcriber : IMessageRecievier<TMessage>;
+        void Broadcast<TMessage>(TMessage message);
+    }
+}
